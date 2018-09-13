bb155ec50e4bad3c8e41afff04a884bbf56e26e1
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-12 22:50:31 -0300

    Add project files.

---
+++ ConsoleRedis/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+namespace ConsoleRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Connect
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+
+
+
+            db.HashSet("P1", "1+1", "2");
+            db.HashSet("P1", "1+2", "3");
+            db.HashSet("P1", "2+2", "4");
+            db.HashSet("P1", "4+1", "5");
+
+
+
+            var sub = redis.GetSubscriber();
+            sub.Subscribe("Oraculo_eh_nois", (ch, msg) => 
+                {
+                    
+                    
+
+
+                    var q = db.HashGet("P1", msg.ToString());
+                    Console.WriteLine(q);
+                }
+            );
+
+            Console.Read();
+        }
+    }
+}
